97fbc4e447ba36c280623e3bea73660d1a3e9edb
joshy198 <joshy198@gmx.at>
2016-03-09 12:08:18 +0100

    Java Project added

---
+++ Maringer_Mayrhofer_android/src/MyFirstProgram.java
@@ -0,0 +1,12 @@
+public class MyFirstProgram {
+	public static void main(String[] args) {
+		int numberOfOutputs; // integer statt int
+		Out.print("Please enter the number of outputs: ");
+		numberOfOutputs = In.readInt(); // Tippfehler
+		for (int i = 1; i <= numberOfOutputs; i++) { // = hat gefehlt
+			Out.print("Output number ");
+			Out.print(i); // j statt i
+			Out.println(": Hello World!");
+		} // for } // main } // MyFirstProgram
+	}
+}
