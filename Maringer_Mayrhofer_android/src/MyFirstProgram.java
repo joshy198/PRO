913fd7b6590ed383a8d7745bc3e87d33183e3965
Qre4Tor <Bastian.Maringer@gmx.net>
2016-03-10 17:12:32 +0100

    Fixxing the bugs

--- Maringer_Mayrhofer_android/src/MyFirstProgram.java
+++ Maringer_Mayrhofer_android/src/MyFirstProgram.java
@@ -17,5 +17,7 @@ public class MyFirstProgram {
 	public static void thisIsANewNotWorkingJavaMethod(int uselessJavaArgument){
 		System.out.println("Java ist voll ober toll.");
 		Out.Print("Useless java argument: "+uselessJavaArgument);
+		int a = 1; //Variable zum fixxen des Bugs
+		System.out.println(a);
 	}
 }
